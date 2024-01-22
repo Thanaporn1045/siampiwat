8ead82c8be5b1991fd4f5bb8f4e817e30020ffbf
หมี โหด <eooes@users.noreply.github.com>
2024-01-22 20:52:39 +0700

    update

--- java/index.js
+++ java/index.js
@@ -18,3 +18,28 @@
     window.location.href = 'P2.html';
   }
 
+
+///////////////// สำหรับรันเวลาไม่ได้แตะหน้าจอ ตั้งเวลาไว้ หนึ่งนาทีครึ่ง///////////////////////////
+function randomscencewhennoonetouch() {
+  var pagealert1 = document.getElementById('randomscence');
+  var link = document.createElement('a');
+  var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+  // link.href = `./P2.html?SetStorageDeckA=S17P${randomNumber2}`;
+  link.href = `./index.html?SetStorageDeckA=S17P${randomNumber2}`;
+  pagealert1.appendChild(link);
+  setTimeout(function() {
+      link.click();
+  }, 1000);
+}
+let touchedscreen = false;
+document.addEventListener('touchstart', function() {
+  touchedscreen = true;
+});
+
+setTimeout(function() {
+  // ตรวจสอบว่าไม่มีการแตะหน้าจอไอแพด
+  if (!touchedscreen) {
+      randomscencewhennoonetouch();
+  }
+}, 5000);
+///////////////// สำหรับรันเวลาไม่ได้แตะหน้าจอ ตั้งเวลาไว้ หนึ่งนาทีครึ่ง///////////////////////////
